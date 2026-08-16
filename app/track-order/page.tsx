9a395ff730cfcb9c031635f8c57dc54d8842b7df
OUTSIDER <OUTSIDER@RENZYs-MacBook-Pro.local>
2026-08-16 02:54:24 -0700

    Add customer order tracking page

---
+++ app/track-order/page.tsx
@@ -0,0 +1,3 @@
+import { OrderTracker } from '@/components/OrderTracker';
+export const metadata = { title: 'Track order', robots: { index: false, follow: false } };
+export default function TrackOrderPage(){return <OrderTracker/>;}
